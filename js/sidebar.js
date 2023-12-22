1312848d20c67fe2965c3a2773912a004d7f370a
Kharommy <kharommy@192.168.1.14>
2023-12-22 13:27:05 +0700

    init

---
+++ js/sidebar.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  const sidebar = $('.sidebar');
+  const sidebarToggle = '#sidebar-toggle';
+  sidebar.load('../layout/sidebar.html');
+
+  // toggle sidebar mobile
+  $(document).on('click', sidebarToggle, function () {
+    sidebar.addClass('active');
+  });
+
+  // remove sidebar
+  const sb = $(sidebarToggle);
+
+  $(document).on('click', function (e) {
+    e.preventDefault();
+
+    if (!sidebar.hasClass('active')) {
+      return;
+    }
+
+    if (
+      !sb.is(':contains(' + e.target.textContent + ')') &&
+      !sidebar.is(':contains(' + e.target.textContent + ')')
+    ) {
+      sidebar.removeClass('active');
+    }
+  });
+});
